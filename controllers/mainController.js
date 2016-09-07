a406094b864f0f96a19c6c98ea08510eb592c4b3
Arda Sahiner <sahinera@berkeley.edu>
2016-09-06 22:15:48 -0700

    making custom audio

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,24 +1,27 @@
 angular.module('mainController', [])
 
-.controller('mainController', function($scope) {
+.controller('mainController', function($window) {
 
   var vm = this;
-  vm.audioList = [document.getElementById("myAudio")];
-  vm.playingList = [false];
-  vm.pausedList = [true];
-  vm.durationList = [0];
-  vm.timeRemainingList = [0];
-  vm.activeAudio = null;
+  vm.audioList = [document.getElementById("audio0"), document.getElementById("audio1"), document.getElementById("audio2")];
+  vm.loadedList = [false, false, false];
+  vm.playingList = [false, false, false];
+  vm.pausedList = [true, true, true];
+  vm.durationList = [0, 0, 0];
+  vm.timeRemainingList = [0, 0, 0];
+  vm.activeAudio = document.getElementById("audio0"); // default to 0
+  vm.activeAudioIndex = 0; // default to 0
 
   vm.aud_play = function(index) {
     if (!vm.playingList[index]) {
 
-      if (vm.activeAudio != null && vm.activeAudio !== vm.audioList[index]) {
+      if (vm.activeAudio != null && vm.activeAudioIndex !== index) {
 
-        vm.activeAudio.pause();
+        vm.aud_pause(vm.activeAudioIndex);
       }
 
       vm.activeAudio = vm.audioList[index];
+      vm.activeAudioIndex = index;
       vm.audioList[index].play();
       vm.playingList[index] = true;
       vm.pausedList[index] = false;
@@ -40,16 +43,38 @@ angular.module('mainController', [])
 
     if (vm.audioList[index].ended) {
 
-      document.getElementById('tracktime').innerHTML = Math.floor((vm.audioList[index].duration) * 10) / 10;
+      var the_id = 'tracktime' + index;
+      document.getElementById(the_id).innerHTML = Math.floor((vm.audioList[index].duration));
     }
   };
 
   vm.getDuration = function() {
     for (var i = 0; i < vm.audioList.length; i ++ ) {
-      vm.durationList[i] = vm.audioList[i].duration;
-      vm.timeRemainingList[i] = Math.floor((vm.audioList[i].duration - vm.audioList[i].currentTime) * 10) / 10;
+      if (vm.audioList[i].readyState >= 1) {
+        vm.durationList[i] = vm.audioList[i].duration;
+      } else {
+        console.log("something happened");
+        vm.durationList[i] = vm.audioList[i].duration;
+      }
+      vm.timeRemainingList[i] = Math.floor(vm.audioList[i].duration - vm.audioList[i].currentTime);
+    }
+
+    for (var i = 0; i < vm.audioList.length; i ++ ) {
+
+      if (isNaN(vm.durationList[i])) {
+        $window.location.reload();
+      } else {
+
+        vm.loadedList[i] = true;
+      }
     }
   }
+
+  function sleep(seconds) {
+    var e = new Date().getTime() + (seconds * 1000);
+    while (new Date().getTime() <= e) {}
+  }
+
   vm.getDuration();
   return vm;
 });
